c467699eb4a5e9b9e6636820bf8a403692ae056e
Yevay <yevhenii.may@gmail.com>
2022-12-19 23:42:44 +0200

    Init

---
+++ src/main/java/com/yevay/remy/RemyApplication.java
@@ -0,0 +1,12 @@
+package com.yevay.remy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemyApplication.class, args);
+	}
+}
