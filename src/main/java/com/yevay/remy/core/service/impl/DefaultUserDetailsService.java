279351c3ea489e0384d5398d5a8cd0155fe140e2
Yevay <yevhenii.may@gmail.com>
2022-12-23 13:07:50 +0200

    Add user details implementation

---
+++ src/main/java/com/yevay/remy/core/service/impl/DefaultUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.yevay.remy.core.service.impl;
+
+import com.yevay.remy.model.repo.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public DefaultUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email [" + email + "] not found"));
+    }
+}
