daf17245068ee021c14726ca12faf10b0636a583
Yevay <yevhenii.may@gmail.com>
2022-12-23 19:33:44 +0200

    Add authentication manager bean

--- src/main/java/com/yevay/remy/config/WebSecurityConfig.java
+++ src/main/java/com/yevay/remy/config/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.yevay.remy.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,6 +28,11 @@ public class WebSecurityConfig {
                 .and()
                 .httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
