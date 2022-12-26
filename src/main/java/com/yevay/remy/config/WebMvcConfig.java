22d4ff8bfe082d91d5e5713e23baad7270a39325
Yevay <yevhenii.may@gmail.com>
2022-12-26 17:26:02 +0200

    Enable headers for cors

--- src/main/java/com/yevay/remy/config/WebMvcConfig.java
+++ src/main/java/com/yevay/remy/config/WebMvcConfig.java
@@ -13,6 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping(mapping)
-                .allowedOrigins(allowedOrigins);
+                .allowedOrigins(allowedOrigins)
+                .allowedHeaders("*");
     }
 }
