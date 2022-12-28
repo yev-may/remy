de099617ef20bb4bd1dd4b8455f812a1c6a6f3d7
Yevay <yevhenii.may@gmail.com>
2022-12-28 21:24:45 +0200

    Change post login redirect path

--- src/main/java/com/yevay/remy/config/WebSecurityConfig.java
+++ src/main/java/com/yevay/remy/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig {
                 .formLogin()
                     .loginPage("/login")
                     .failureUrl("/login?error=true")
-                    .defaultSuccessUrl("/", true)
+                    .defaultSuccessUrl("/workspace/all", true)
                 .and()
                 .logout().logoutUrl("/logout").deleteCookies("JSESSIONID")
                 .and()
