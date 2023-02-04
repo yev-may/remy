050db08ad30612570bf929dc299216c9bf22484b
Yevay <yevhenii.may@gmail.com>
2023-02-04 23:37:04 +0200

    Remove unused imports

--- src/main/java/com/yevay/remy/core/facade/impl/DefaultJwtAuthFacade.java
+++ src/main/java/com/yevay/remy/core/facade/impl/DefaultJwtAuthFacade.java
@@ -8,13 +8,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class DefaultJwtAuthFacade implements JwtAuthFacade {
